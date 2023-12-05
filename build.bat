576b93e98c65b264b5bf116aa8afbd9b713154ac
lefty <leftcausedit@gmail.com>
2023-12-05 19:32:41 +0800

    ignore custom_spider.jar

--- build.bat
+++ build.bat
@@ -4,4 +4,8 @@ call "%~dp0\gradlew" assembleRelease --no-daemon
 
 call "%~dp0\jar\genJar.bat" %1
 
+copy "%~dp0\jar\custom_spider.jar" "%~dp0\jat\lefty.jar"
+
+copy "%~dp0\jar\lefty.jar" "D:\filestation\config\tvbox\jar\"
+
 pause
